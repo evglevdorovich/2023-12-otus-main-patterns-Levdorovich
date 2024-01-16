14767306c834e874b8d0e4338c89ecac61cc2723
evglevdorovich <lev_jack@mail.ru>
2024-01-16 10:39:50 +0100

    add pom to test

---
+++ quadratics/src/test/java/ru/otus/QuadraticsServiceTest.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuadraticsServiceTest {
+    @Test
+    void test(){
+        assertNotNull(null);
+    }
+
+}
