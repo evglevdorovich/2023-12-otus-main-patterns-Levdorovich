e7f67e3039e6ddfec3ad88c3f0c27f6b9f6f4eb9
evglevdorovich <lev_jack@mail.ru>
2024-01-18 00:33:27 +0100

    add quadratics solve realisation

--- quadratics/src/main/java/ru/otus/QuadraticsService.java
+++ quadratics/src/main/java/ru/otus/QuadraticsService.java
@@ -1,5 +1,41 @@
 package ru.otus;
 
 public class QuadraticsService {
+    private static final double EPS = 1e-5;
 
+    public double[] solve(double a, double b, double c) {
+
+        validateNumbers(a, b, c);
+
+        var discriminant = calculateDiscriminant(a, b, c);
+
+        if (Math.abs(discriminant) < EPS) {
+            var result = -b / (2 * a);
+            return new double[]{result, result};
+        }
+        if (discriminant < 0) {
+            return new double[0];
+        } else {
+            return new double[]{(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)};
+        }
+    }
+
+    private static void validateNumbers(double a, double b, double c) {
+        if (Math.abs(a) < EPS) {
+            throw new IllegalArgumentException("a cannot be less then zero, a = " + a);
+        }
+
+        if (Double.isInfinite(a) || Double.isInfinite(b) || Double.isInfinite(c)) {
+            throw new IllegalArgumentException("1 or more of the arguments are infinite");
+
+        }
+
+        if (Double.isNaN(a) || Double.isNaN(b) || Double.isNaN(c)) {
+            throw new IllegalArgumentException("1 or more of the arguments are NaN");
+        }
+    }
+
+    private static double calculateDiscriminant(double a, double b, double c) {
+        return b * b - (4.0 * a * c);
+    }
 }
--- quadratics/src/test/java/ru/otus/QuadraticsServiceTest.java
+++ quadratics/src/test/java/ru/otus/QuadraticsServiceTest.java
@@ -2,12 +2,85 @@ package ru.otus;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class QuadraticsServiceTest {
+    private final QuadraticsService quadraticsService = new QuadraticsService();
+
     @Test
-    void test(){
-        assertNotNull(null);
+    void shouldReturnNoSqrt() {
+        var a = 1;
+        var b = 0;
+        var c = 1;
+
+        var expectedResult = new double[0];
+
+        var actualResult = quadraticsService.solve(a, b, c);
+
+        assertThat(actualResult).containsExactlyInAnyOrder(expectedResult);
+    }
+
+    @Test
+    void shouldReturn2Results() {
+        var a = 1;
+        var b = 0;
+        var c = -1;
+
+        var expectedResult = new double[]{1, -1};
+
+        var actualResult = quadraticsService.solve(a, b, c);
+
+        assertThat(actualResult).containsExactlyInAnyOrder(expectedResult);
+    }
+
+    @Test
+    void shouldReturn2EqualResults() {
+        var a = 1;
+        var b = 2;
+        var c = 1;
+
+        var expectedResult = new double[]{-1, -1};
+
+        var actualResult = quadraticsService.solve(a, b, c);
+
+        assertThat(actualResult).containsExactlyInAnyOrder(expectedResult);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionIfAIsInErrorRange() {
+        var a = 1e-6;
+        var b = 2;
+        var c = 1;
+
+        assertThatThrownBy(() -> quadraticsService.solve(a, b, c)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionIfNaN() {
+        var a = Double.NaN;
+        var b = 2;
+        var c = 1;
+
+        assertThatThrownBy(() -> quadraticsService.solve(a, b, c)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionIfPositiveInfinity() {
+        var a = Double.POSITIVE_INFINITY;
+        var b = 2;
+        var c = 1;
+
+        assertThatThrownBy(() -> quadraticsService.solve(a, b, c)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionIfNegativeInfinity() {
+        var a = Double.NEGATIVE_INFINITY;
+        var b = 2;
+        var c = 1;
+
+        assertThatThrownBy(() -> quadraticsService.solve(a, b, c)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
