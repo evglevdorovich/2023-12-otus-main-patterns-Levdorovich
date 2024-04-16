e17b19dc96f7b89a8f34e378a845e7b49deed219
evglevdorovich <lev_jack@mail.ru>
2024-04-16 18:36:09 +0200

    add move collision factory to show test cases

--- spaceship/src/main/java/com/example/spaceship/factory/CommandMoveFactory.java
+++ spaceship/src/main/java/com/example/spaceship/factory/CommandMoveFactory.java
@@ -5,6 +5,7 @@ import com.example.spaceship.command.CheckFuelCommand;
 import com.example.spaceship.command.Command;
 import com.example.spaceship.command.MacroCommand;
 import com.example.spaceship.command.MoveCommand;
+import com.example.spaceship.model.Field;
 import com.example.spaceship.model.FuelConsumer;
 import com.example.spaceship.model.Movable;
 import lombok.RequiredArgsConstructor;
@@ -12,22 +13,24 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Queue;
 
 @Component
 @RequiredArgsConstructor
 public class CommandMoveFactory {
+    private final CommandCollisionFactory commandCollisionFactory;
 
-
-    public MacroCommand buildMoveAndBurnFuelCommand(Movable movable) {
+    public MacroCommand buildMoveAndBurnFuelCommand(Queue<Command> commandQueue, Field field, Movable movable, int maxGameObjectDiameter) {
         var commands = new ArrayList<Command>();
 
-        if (movable instanceof FuelConsumer) {
-            commands.add(new CheckFuelCommand((FuelConsumer) movable));
+        if (movable instanceof FuelConsumer fuelConsumer) {
+            commands.add(new CheckFuelCommand(fuelConsumer));
             commands.add(new MoveCommand(movable));
-            commands.add(new BurnFuelCommand((FuelConsumer) movable));
+            commands.add(new BurnFuelCommand(fuelConsumer));
         } else {
             commands.add(new MoveCommand(movable));
         }
+        commands.add(commandCollisionFactory.createCollisionCommand(commandQueue, field, movable, maxGameObjectDiameter));
         return new MacroCommand(Collections.unmodifiableList(commands));
     }
 }
--- spaceship/src/test/java/com/example/spaceship/factory/CommandMoveFactoryTest.java
+++ spaceship/src/test/java/com/example/spaceship/factory/CommandMoveFactoryTest.java
@@ -5,32 +5,48 @@ import com.example.spaceship.command.CheckFuelCommand;
 import com.example.spaceship.command.Command;
 import com.example.spaceship.command.MacroCommand;
 import com.example.spaceship.command.MoveCommand;
+import com.example.spaceship.model.Dimension;
+import com.example.spaceship.model.Field;
 import com.example.spaceship.model.FuelConsumer;
 import com.example.spaceship.model.Movable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CommandMoveFactoryTest {
     @Mock
     private MovableFuelConsumer movable;
+    @Mock
+    private CommandCollisionFactory commandCollisionFactory;
+    @InjectMocks
+    private CommandMoveFactory commandMoveFactory;
 
     @Test
     void shouldReturnMoveAndBurnFuelCommand() {
-        var commandMoveFactory = new CommandMoveFactory();
         FuelConsumer fuelConsumer = movable;
+        var field = new Field(new Dimension(List.of()), Map.of());
+        var queue = new ArrayDeque<Command>();
+        var maxGameObjectDiameter = 4;
+        var commandCollisionMacroCommand = new MacroCommand(List.of());
+
         List<Command> moveAndBurnFuelCommands = List.of(new CheckFuelCommand(fuelConsumer), new MoveCommand(movable),
-        new BurnFuelCommand(fuelConsumer));
+        new BurnFuelCommand(fuelConsumer), commandCollisionMacroCommand);
+
+        when(commandCollisionFactory.createCollisionCommand(queue, field, movable, maxGameObjectDiameter)).thenReturn(commandCollisionMacroCommand);
 
         var expectedMoveAndBurnFuelCommand = new MacroCommand(moveAndBurnFuelCommands);
 
-        var actualMoveAndBurnFuelCommand = commandMoveFactory.buildMoveAndBurnFuelCommand(movable);
+        var actualMoveAndBurnFuelCommand = commandMoveFactory.buildMoveAndBurnFuelCommand(queue, field, movable, maxGameObjectDiameter);
 
         assertThat(actualMoveAndBurnFuelCommand).isEqualTo(expectedMoveAndBurnFuelCommand);
     }
